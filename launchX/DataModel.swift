647843c3882f15eef0b45897b37fdf7ddf65b28a
Chuabacca <jonathan.chua@dollarshaveclub.com>
2017-10-26 12:01:26 -0700

    Added launch sites dictionary to a data model file.

---
+++ launchX/DataModel.swift
@@ -0,0 +1,22 @@
+//
+//  DataModel.swift
+//  launchX
+//
+//  Created by Jonathan Chua on 10/26/17.
+//  Copyright © 2017 Jonathan Chua. All rights reserved.
+//
+
+import Foundation
+
+class LaunchSites {
+    let name = [
+        "kwajalein_atoll":"Omelek Island, Marshal Islands",
+        "ccafs_slc_40":"Cape Canaveral Air Force Station Space Launch Complex 40, Florida",
+        "ccafs_lc_13":"Cape Canaveral Air Force Station Space Launch Complex 13, Florida",
+        "ksc_lc_39a":"Kennedy Space Center Launch Complex 39A, Cape Canaceral, Florida",
+        "vafb_slc_3w":"Vandenberg Air Force Base Space Launch Complex 3W, California",
+        "vafb_slc_3w":"Vandenberg Air Force Base Space Launch Complex 3W, California",
+        "vafb_slc_4w":"Vandenberg Air Force Base Space Launch Complex 4W, California",
+        "stls":"SpaceX South Texas Launch Site, Boca Chica Village, Texas"
+    ]
+}
